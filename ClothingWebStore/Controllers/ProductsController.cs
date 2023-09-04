bde5c43f824eb94db1e0c4460a42b9c653e6c310
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2023-09-04 17:25:26 +0200

    Add project files.

---
+++ ClothingWebStore/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ClothingWebStore.Models;
+using ClothingWebStore.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothingWebStore.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService _productService { get; }
+        public ProductsController(JsonFileProductService productService)
+        {
+            this._productService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _productService.GetProducts();
+        }
+    }
+}
---
+++ ClothingWebStore/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using ClothingWebStore.Models;
+using ClothingWebStore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClothingWebStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService _productService;
+
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = _productService.GetProducts();
+        }
+    }
+}
